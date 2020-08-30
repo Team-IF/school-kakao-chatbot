05cb4a8c1fa64d8ef350b7f2a2d64b8ce572a669
Coder-Iro <Coder-Iro@teamif.io>
2020-08-31 01:48:23 +0900

    Just.. Just... I don't know what I did

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from comcigan import School
 APP = Flask(__name__)
 API = Api(APP)
 
+
 class timetable(Resource):
     @staticmethod
     def post():
@@ -21,6 +22,7 @@ class carte(Resource):
     def post():
         parser = reqparse.RequestParser()
         parser.add_argument('userRequest', type=dict)
+
         parser.add_argument('bot', type=dict)
         args = parser.parse_args()
         userinfo = args['userRequest']
@@ -36,4 +38,3 @@ class register(Resource):
         args = parser.parse_args()
         userinfo = args['userRequest']
         botinfo = args['bot']
-        
